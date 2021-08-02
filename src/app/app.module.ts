e2e138150d571d0e2cb4b05617ad5c65c95c11ca
jalo-git <jesselleanne.ocampo@gmail.com>
2021-08-02 19:51:37 +0800

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { JobsComponent } from './users/jobs/jobs.component';
+import { TermsComponent } from './users/terms/terms.component';
+import { UsersDashboardComponent } from './users/users-dashboard/users-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent,
+    JobsComponent,
+    TermsComponent,
+    UsersDashboardComponent,
   ],
   imports: [
     BrowserModule,
